0bb82c294e6ebbb8d61502e252caf25b97934aed
Sinan Barut <s.barut@tu-bs.de>
2021-08-30 16:41:14 +0200

    Implement fully functional IK
    But the structue is chaotic

--- src/ccc.cpp
+++ src/ccc.cpp
@@ -29,7 +29,6 @@ CCCMechanism::forward_verbose(const Configuration &config) const {
 
 std::vector<Configuration>
 CCCMechanism::inverse(const DualFrame &pose) const {
-
     auto s = pose * this->zero_posture.inverse();
 
     DualSkew crossterm(this->l23);
@@ -40,37 +39,86 @@ CCCMechanism::inverse(const DualFrame &pose) const {
     DualNumber b = this->l12 * (crossterm * this->l34);
     DualNumber c = this->l12 * ((s - squareterm) * this->l34);
 
-    auto phi2_solutions = solve_trigonometric_equation(a, b, c);
-    bool enable_offset = false;
-
-    if (phi2_solutions.empty()) {
-        auto p = s * this->l34;
-        auto d = cross(this->l12.n(), this->l12.m() - p.m()).norm();
-
-        phi2_solutions.emplace_back(DualNumber(0,-d));
-        phi2_solutions.emplace_back(DualNumber(0,d));
-
-        enable_offset = true;
-    }
+    DualNumber distance = this->l12.get_distance(s * this->l34);
+    std::vector<DualNumber> phi2_solutions = solve_trigonometric_equation(a, b, c);
 
     std::vector<Configuration> solutions;
 
     for (auto phi_2 : phi2_solutions) {
-        DualFrame m2(DualSkewProduct(this->l23, phi_2));
+        // coincide
+        if ( (abs(distance.real()) < 0.000001 && distance.dual() < 0.000001) || (abs(distance.real()-M_PI) < 0.00001 && abs(distance.dual()) < 0.00001)) {
 
-        auto phi_1 = (-this->l12).acos3(s * this->l34, m2 * this->l34);
-        auto phi_3 = this->l34.acos3(s.inverse() * this->l12, m2.inverse() * this->l12);
+            if (abs(distance.real()-M_PI) < 0.0001) {
+                phi_2 = phi_2 + M_PI;
+            }
+            DirectionVector u(cross(this->l12.n(), this->l23.n()));
+            UnitLine orthogonal(
+                u.normal(),
+                PointVector(0,0,0)
+            );
 
-        // TODO change hack
-        if (enable_offset) {
-            auto intermediate_line = DualFrame(DualSkewProduct(this->l12,phi_1)) * m2 * this->l23;
-            auto offset = l34.acos3(
-                    intermediate_line,
-                    intermediate_line.parallel_through_anchor(s.p()));
-            phi_3 = phi_3 + offset;
+            DualFrame m2(DualSkewProduct(this->l23, phi_2));
+            auto phi_1 = (-this->l12).acos3(s * orthogonal, m2 * orthogonal);
+            auto phi_3 = DualNumber();
+
+            solutions.push_back({phi_1, phi_2, phi_3});
+        } else {
+            // parallel
+            if ( abs(distance.real()) < 0.000001 || abs(distance.real()-M_PI) < 0.000001) {
+                DualNumber flip;
+                if ( abs(distance.real()-M_PI) < 0.00001) {
+                    flip = M_PI;
+                }
+                DualFrame m2(DualSkewProduct(this->l23, phi_2));
+                DualFrame m2_rot(DualSkewProduct(this->l23, phi_2.real()));
+                auto phi_1 = (-this->l12).acos3(s * this->l34, m2 * this->l34);
+                auto phi_3 = this->l34.acos3(s.inverse() * this->l12, m2.inverse() * this->l12);
+
+                DualFrame m1_combined(DualSkewProduct(this->l12,(phi_1 + phi_3).real()));
+                auto pre_pose = m1_combined * m2_rot * this->zero_posture;
+
+                // Some kind of Paden-Kahan Problem
+                PointVector anchor = this->l12.get_canonical_anchor();
+                PointVector p_target = this->l12.orthogonal_plane_projection(anchor,pose.p());
+                PointVector p_begin = this->l12.orthogonal_plane_projection(anchor, (pre_pose).p());
+
+                PointVector o_o = this->l12.get_canonical_anchor();
+                PointVector o_r = (m1_combined * m2_rot * this->l34).point_project(o_o);
+
+                Vector p = p_target - p_begin;
+                Vector o = o_r - o_o;
+                Vector n = (m1_combined * this->l23).n();
+                Vector w = this->l12.n();
+
+                double minus_p_2 = -(n * o);
+                double q = - (p * ( p + 2 * o));
+                double sq = sqrt(minus_p_2 * minus_p_2 - q);
+                std::vector<double> alphas = {minus_p_2 - sq, minus_p_2 + sq};
+
+                Vector v = p + o;
+                for (double alpha : alphas) {
+                    Vector u = o + alpha * n;
+                    double phi_off = atan2(w * cross(u,v), u * v);
+                    auto tmp_translation = this->l12.point_project(pose.p()) - this->l12.point_project((m2 * this->zero_posture).p());
+                    double translation_off = tmp_translation.norm() * (tmp_translation * this->l12.n() > 0 ? 1.0 : -1.0);
+
+                    DualFrame m2_n(DualSkewProduct(this->l23, DualNumber(phi_2.real(), alpha) + flip));
+                    auto correction = this->l34.acos3(s.inverse() * this->l12, m2_n.inverse() * this->l12);
+
+                    solutions.push_back({
+                        phi_1 + phi_3 + DualNumber(phi_off, translation_off),
+                        DualNumber(phi_2.real(), alpha) + flip,
+                        correction
+                    });
+                }
+                //generic
+            } else {
+                DualFrame m2(DualSkewProduct(this->l23, phi_2));
+                auto phi_1 = (-this->l12).acos3(s * this->l34, m2 * this->l34);
+                auto phi_3 = this->l34.acos3(s.inverse() * this->l12, m2.inverse() * this->l12);
+                solutions.push_back({phi_1, phi_2, phi_3});
+            }
         }
-
-        solutions.push_back({phi_1, phi_2, phi_3});
     }
 
     return solutions;
--- test/ccc_test.cpp
+++ test/ccc_test.cpp
@@ -23,42 +23,190 @@
 
 #include <gtest/gtest.h>
 
-#define EXPECT_NEAR_DN(x, y, e) \
-EXPECT_NEAR((x).real(), (y).real(), e);\
-EXPECT_NEAR((x).dual(), (y).dual(), e)
-
 using namespace DualNumberAlgebra;
 using namespace DualNumberAlgebra::literals;
 
-TEST(Mechanism, SU_Fixed_Frames) { // NOLINT
-
-    UnitLine a(
+class Mechanism : public testing::Test {
+protected:
+    void SetUp() override {
+    UnitLine z(
             UnitDirectionVector(0,0,1),
             PointVector(0,0,0)
-            );
+    );
 
-    UnitLine b(
+    UnitLine x(
             UnitDirectionVector(1,0,0),
             PointVector(0,0,0)
     );
 
-    UnitLine c(
+    DualFrame su_zp(
+            RotationMatrix(0,0,0),
+            PointVector(0,0,0)
+    );
+
+    SU = std::make_unique<CCCMechanism>(z,x,z,su_zp);
+
+
+
+
+
+
+    }
+
+    std::vector<DualFrame> frames;
+
+    std::unique_ptr<CCCMechanism> SU;
+    std::unique_ptr<CCCMechanism> orthogonal;
+    std::unique_ptr<CCCMechanism> arbitrary;
+};
+
+// Helper function
+
+CCCMechanism create_SU() {
+    UnitLine z(
             UnitDirectionVector(0,0,1),
             PointVector(0,0,0)
     );
 
-    DualFrame zp(RotationMatrix(0,0,0), PointVector(0,0,0));
+    UnitLine x(
+            UnitDirectionVector(1,0,0),
+            PointVector(0,0,0)
+    );
 
-    CCCMechanism m(a,b,c,zp);
+    DualFrame su_zp(
+            RotationMatrix(0,0,0),
+            PointVector(0,0,0)
+    );
+
+    return CCCMechanism(z,x,z,su_zp);
+}
+
+void systematic(CCCMechanism &m) {
+    for (int rz_i = -2; rz_i < 2; rz_i++) {
+        for (int ry_i = -2; ry_i < 2; ry_i++) {
+            for (int rx_i = -2; rx_i < 2; rx_i++) {
+                for (int z_i = -2; z_i < 2; z_i++) {
+                    for (int y_i = -2; y_i < 2; y_i++) {
+                        for (int x_i = -2; x_i < 2; x_i++) {
+                            DualFrame frame(
+                                    RotationMatrix(rz_i * M_PI_4, ry_i * M_PI_4, rx_i * M_PI_4),
+                                    PointVector(x_i, y_i, z_i)
+                            );
+                            auto configs = m.inverse(frame);
+
+                            for (const auto &config : configs) {
+                                auto fk = m.forward(config);
+                                EXPECT_EQ(fk, frame) << "(p: " << x_i << ", " << y_i << ", " << z_i << ")" << std::endl
+                                                     << "(R: " << rz_i << ", " << ry_i << ", " << rx_i << ")" << std::endl;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+TEST(Mechanism, SU_Fixed_Frames) { // NOLINT
+    CCCMechanism su = create_SU();
 
     std::vector<DualFrame> frames = {
-            DualFrame(RotationMatrix(0,0,0), PointVector(0,0,0))
+            DualFrame(RotationMatrix(0,0,0), PointVector(0,0,0)),
+            DualFrame(RotationMatrix(0,0,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(2,0,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(M_PI,0,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(M_PI_2,0,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(0,M_PI_2,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(M_PI_2,M_PI_2,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(0,M_PI,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(M_PI,M_PI,0), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(1,M_PI,2), PointVector(0,0,-5)),
+            DualFrame(RotationMatrix(0,0,0), PointVector(1,0,-5)),
+            DualFrame(RotationMatrix(2,0,0), PointVector(0,1,-5)),
+            DualFrame(RotationMatrix(M_PI,0,0), PointVector(2,0,-5)),
+            DualFrame(RotationMatrix(M_PI_2,0,0), PointVector(0,2,-5)),
+            DualFrame(RotationMatrix(0,M_PI_2,0), PointVector(3,0,-5)),
+            DualFrame(RotationMatrix(M_PI_2,M_PI_2,0), PointVector(0,3,-5)),
+            DualFrame(RotationMatrix(0,M_PI,0), PointVector(4,0,-5)),
+            DualFrame(RotationMatrix(M_PI,M_PI,0), PointVector(0,4,-5)),
+            DualFrame(RotationMatrix(1,M_PI,2), PointVector(5,5,-5)),
+            DualFrame(RotationMatrix(M_PI_4, 0, M_PI_4), PointVector(1, 0, 0))
     };
 
     for (const auto &frame : frames) {
-        auto configs = m.inverse(frame);
+        auto configs = su.inverse(frame);
+
         for (const auto &config : configs) {
-            auto fk = m.forward(config);
+            auto fk = su.forward(config);
+            EXPECT_EQ(fk, frame);
+        }
+    }
+}
+
+TEST(Mechanism, SU_Systematic_Frames) { // NOLINT
+    auto mechanism = create_SU();
+    systematic(mechanism);
+}
+
+TEST(Mechanism, Orthogonal_Systematic_Frames) {
+    UnitLine a(
+            DirectionVector(1,0,1).normal(),
+            PointVector(0,0,0)
+    );
+
+    UnitLine b(
+            DirectionVector(0,1,0).normal(),
+            PointVector(1,0,0)
+    );
+
+    UnitLine c(
+            DirectionVector(1,0,0).normal(),
+            PointVector(0,-4,1)
+    );
+
+    DualFrame zp(
+            RotationMatrix(1 * M_PI_4, -1 * M_PI_4, 3 * M_PI_4),
+            PointVector(-2,0,4)
+    );
+
+    CCCMechanism orthogonal(a, b, c, zp);
+    systematic(orthogonal);
+}
+
+TEST(Mechanism, Orthogonal_Enforced_Frames) { // NOLINT
+    UnitLine a(
+            DirectionVector(1,0,1).normal(),
+            PointVector(0,0,0)
+    );
+
+    UnitLine b(
+            DirectionVector(0,1,0).normal(),
+            PointVector(1,0,0)
+    );
+
+    UnitLine c(
+            DirectionVector(1,0,0).normal(),
+            PointVector(0,2,M_SQRT1_2)
+    );
+
+    DualFrame zp(
+            RotationMatrix(1 * M_PI_4, -1 * M_PI_4, 3 * M_PI_4),
+            PointVector(-2,0,4)
+    );
+
+    CCCMechanism orthogonal(a,b,c,zp);
+
+    std::vector<Configuration> configs = {
+            {0, -2_s, 0}, // Intersecting
+            {0, -M_PI_4, 0}, // Parallel
+            {0, -M_PI_4-2_s,0} // Coinciding
+    };
+
+    for (auto config : configs) {
+        auto frame = orthogonal.forward(config);
+        auto inverse_configs = orthogonal.inverse(frame);
+        for (const auto &inverse_config : inverse_configs) {
+            auto fk = orthogonal.forward(config);
             EXPECT_EQ(fk, frame);
         }
     }
