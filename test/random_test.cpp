0a8834c32929ce267cd2f7578209640ba53b41e7
Sinan Barut <s.barut@tu-bs.de>
2021-11-22 11:23:34 +0100

    Add naive test

---
+++ test/random_test.cpp
@@ -0,0 +1,33 @@
+//
+// Created by sba on 22.11.21.
+//
+
+#include "util/random.h"
+#include <gtest/gtest.h>
+
+// Just check, if they don't throw an exception
+TEST(Random, Typecheck) {
+    try {
+        auto l = Random::SampleLine();
+    } catch(...) {
+        FAIL();
+    }
+
+    try{
+        auto d = Random::SampleDirection();
+    } catch(...) {
+        FAIL();
+    }
+
+    try{
+        auto o = Random::SampleOrientation();
+    } catch(...) {
+        FAIL();
+    }
+
+    try{
+        auto f = Random::SampleFrame();
+    } catch(...) {
+        FAIL();
+    }
+}
